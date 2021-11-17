a27e5e40595e8026915948652b73408b394c9830
Mees Janmaat <meesjanmaat@live.nl>
2021-11-17 10:48:12 +0100

    Update meting.py
    now works with several files at once and stored the results in a list

--- meting.py
+++ meting.py
@@ -4,19 +4,52 @@ import numpy as np
 import matplotlib.pyplot as plt
 from packages import nsp2exp
 
-minima, minima_err, maxima, maxima_err = nsp2exp.find_tops(
-    "metingen 15-11-21/ALL0002/F0002CH3.csv", True
-)
-
 calib, calib_err = nsp2exp.calibrate("metingen 15-11-21/ALL0001/F0001CH1.csv")
 
-valley_index = int(input("Type the number of the valley of interest: "))
-peak_index = int(input("Type the number of the peak of interest: "))
 
-delta_t = abs(minima[valley_index] - maxima[peak_index])
-delta_t_err = np.sqrt(minima_err[valley_index] ** 2 + maxima_err[peak_index] ** 2)
+def get_energy(filename):
+    """Calculates energy difference between peaks for F = 1 with m_F = -1 and m_F = 1 using a given dataset
 
-delta_E = calib * delta_t
-delta_E_err = np.sqrt((delta_t * calib_err) ** 2 + (calib * delta_t_err) ** 2)
+    Args:
+        filename (string): name of csv file
 
-print(f"delta_E = {delta_E} +- {delta_E_err} MHz")
+    Returns:
+        (tuple): tuple containing:
+            delta_E (float): value of energy difference in MHz
+            delta_E_err (float): error of energy difference in MHz"""
+    minima, minima_err, maxima, maxima_err = nsp2exp.find_tops(filename, True)
+
+    valley_index = int(
+        input("[get_energy()] Type the number of the valley of interest: ")
+    )
+    peak_index = int(input("[get_energy()] Type the number of the peak of interest: "))
+
+    delta_t = abs(minima[valley_index] - maxima[peak_index])
+    delta_t_err = np.sqrt(minima_err[valley_index] ** 2 + maxima_err[peak_index] ** 2)
+
+    delta_E = calib * delta_t
+    delta_E_err = np.sqrt((delta_t * calib_err) ** 2 + (calib * delta_t_err) ** 2)
+
+    print(f"[get_energy()] delta_E = {delta_E} +- {delta_E_err} MHz (file: {filename})")
+    return (delta_E, delta_E_err)
+
+
+files = [
+    "metingen 15-11-21/ALL0002/F0002CH3.csv",
+    "metingen 15-11-21/ALL0003/F0003CH3.csv",
+    "metingen 15-11-21/ALL0004/F0004CH3.csv",
+    "metingen 15-11-21/ALL0005/F0005CH3.csv",
+    "metingen 15-11-21/ALL0006/F0006CH3.csv",
+    "metingen 15-11-21/ALL0007/F0007CH3.csv",
+    "metingen 15-11-21/ALL0008/F0008CH3.csv",
+    "metingen 15-11-21/ALL0009/F0009CH3.csv",
+    "metingen 15-11-21/ALL0010/F0010CH3.csv",
+    "metingen 15-11-21/ALL0011/F0011CH3.csv",
+    "metingen 15-11-21/ALL0012/F0012CH3.csv",
+    "metingen 15-11-21/ALL0013/F0013CH3.csv",
+    "metingen 15-11-21/ALL0014/F0014CH3.csv",
+    "metingen 15-11-21/ALL0015/F0015CH3.csv",
+    "metingen 15-11-21/ALL0016/F0016CH3.csv",
+    "metingen 15-11-21/ALL0017/F0017CH3.csv",
+]
+get_energy("metingen 15-11-21/ALL0002/F0002CH3.csv")
